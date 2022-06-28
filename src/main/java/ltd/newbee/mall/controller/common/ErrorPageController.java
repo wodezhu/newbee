9ccb8ae98b67bb289222b68b8c277d50296403ff
zoya <1040375654@qq.com>
2022-06-28 14:55:07 +0800

    newbee commit

---
+++ src/main/java/ltd/newbee/mall/controller/common/ErrorPageController.java
@@ -0,0 +1,99 @@
+/**
+ * 严肃声明：
+ * 开源版本请务必保留此注释头信息，若删除我方将保留所有法律责任追究！
+ * 本软件已申请软件著作权，受国家版权局知识产权以及国家计算机软件著作权保护！
+ * 可正常分享和学习源码，不得用于违法犯罪活动，违者必究！
+ * Copyright (c) 2020 十三 all rights reserved.
+ * 版权所有，侵权必究！
+ */
+package ltd.newbee.mall.controller.common;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+@Controller
+public class ErrorPageController implements ErrorController {
+
+    private static ErrorPageController errorPageController;
+
+    @Autowired
+    private ErrorAttributes errorAttributes;
+
+    private final static String ERROR_PATH = "/error";
+
+    public ErrorPageController(ErrorAttributes errorAttributes) {
+        this.errorAttributes = errorAttributes;
+    }
+
+    public ErrorPageController() {
+        if (errorPageController == null) {
+            errorPageController = new ErrorPageController(errorAttributes);
+        }
+    }
+
+    @RequestMapping(value = ERROR_PATH, produces = "text/html")
+    public ModelAndView errorHtml(HttpServletRequest request) {
+        HttpStatus status = getStatus(request);
+        if (HttpStatus.BAD_REQUEST == status) {
+            return new ModelAndView("error/error_400");
+        } else if (HttpStatus.NOT_FOUND == status) {
+            return new ModelAndView("error/error_404");
+        } else {
+            return new ModelAndView("error/error_5xx");
+        }
+    }
+
+    @RequestMapping(value = ERROR_PATH)
+    @ResponseBody
+    public ResponseEntity<Map<String, Object>> error(HttpServletRequest request) {
+        Map<String, Object> body = getErrorAttributes(request, getErrorAttributeOptions());
+        HttpStatus status = getStatus(request);
+        return new ResponseEntity<Map<String, Object>>(body, status);
+    }
+
+
+    public String getErrorPath() {
+        return ERROR_PATH;
+    }
+
+
+    protected ErrorAttributeOptions getErrorAttributeOptions() {
+        ErrorAttributeOptions of = ErrorAttributeOptions.of(ErrorAttributeOptions.Include.MESSAGE,
+                ErrorAttributeOptions.Include.STACK_TRACE,
+                ErrorAttributeOptions.Include.EXCEPTION);
+        return of;
+    }
+
+
+    protected Map<String, Object> getErrorAttributes(HttpServletRequest request, ErrorAttributeOptions includeStackTrace) {
+        WebRequest webRequest = new ServletWebRequest(request);
+        return this.errorAttributes.getErrorAttributes(webRequest, includeStackTrace);
+    }
+
+    private HttpStatus getStatus(HttpServletRequest request) {
+        Integer statusCode = (Integer) request
+                .getAttribute("javax.servlet.error.status_code");
+        if (statusCode != null) {
+            try {
+                return HttpStatus.valueOf(statusCode);
+            } catch (Exception ex) {
+            }
+        }
+        return HttpStatus.INTERNAL_SERVER_ERROR;
+    }
+}
